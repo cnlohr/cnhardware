9347f7edc79f4417dc5027f34f5efde43213d847
cnlohr <lohr85@gmail.com>
2025-03-01 13:58:00 -0800

    Bump, a little tighter code

--- ch32v006/badapple006/oledtest/oledtest.c
+++ ch32v006/badapple006/oledtest/oledtest.c
@@ -13,6 +13,8 @@
 
 #define SSD1306_RST_PIN PC3
 
+#define I2CDELAY_FUNC
+
 #include "ssd1306_i2c_bitbang.h"
 #include "ssd1306.h"
 
@@ -30,18 +32,10 @@ int main()
 //	funDigitalWrite( PC3, 1 );
 	//funPinMode( PC0, GPIO_CFGLR_OUT_10Mhz_PP ); // VDD On
 	//funDigitalWrite( PC0, 1 );
-	Delay_Ms(100);
+	Delay_Ms(1);
 	ssd1306_rst();
 	ssd1306_i2c_setup();
-	Delay_Ms(100);
-	if( ssd1306_init() )
-	{
-		printf( "OLED FAILURE\n" );
-	}
-	else
-	{
-		printf( "OLED OK\n" );
-	}
+	Delay_Ms(1);
 
 	static const uint8_t ssd1306_init_array[] =
 	{
@@ -62,30 +56,15 @@ int main()
 		0xA6, // Normal (a6)/inverse (a7)
 		0x8D, 0x14, // Set Charge Pump
 		0xAF, // Display On
+		SSD1306_PAGEADDR, 0, 7, // Page setup, start at 0 and end at 7
 	};
 
-#if 0
-	int i;
-	for( i = 0; i < sizeof( ssd1306_init_array ); i++ )
-	{
-		int r = ssd1306_cmd( ssd1306_init_array[i] );
-		if( r )
-		{
-			printf( "Failed to send OLED command at %d\n", i );
-		}
-	}
-#else
 	// Trying another mode
 	if( ssd1306_pkt_send( ssd1306_init_array, sizeof(ssd1306_init_array), 1 ) )
 	{
-		printf( "Failed to setup\n" );
+		puts( "Failed to setup\n" );
 	}
 
-	ssd1306_cmd(SSD1306_PAGEADDR);
-	ssd1306_cmd(0); // Page start address (0 = reset)
-	ssd1306_cmd(7); // Page end address
-
-#endif
 	//int x, y;
 	int t = 0;
 
@@ -101,22 +80,6 @@ int main()
 
 	while(1)
 	{
-
-		#if 0
-		for( y = 0; y < 64; y++ )
-		{
-			for( x = 0; x < 64; x+=8 )
-			{
-				ssd1306_buffer[(x>>3)+y*64/8] =
-					 //(((t)+(x>>3))&1)?0xaa:0x55;
-					//((t & 0x3f) == y)?0xff:0;
-					1<<(t&7);
-			}
-		}
-		#endif
-
-		//memset( ssd1306_buffer, 0, sizeof( ssd1306_buffer ) );
-
 		uint32_t sz = sizeof( ssd1306_buffer )/4;
 		uint32_t i;
 		for( i = 0; i < sz; i++ )
@@ -149,7 +112,7 @@ int main()
 		if( profile )
 		{
 			char st[12];
-			sprintf( st, "%ld", freeTime );
+			mini_itoa( freeTime, 10, 0, 1, st );
 			ssd1306_drawstr( 0, 0, st, 1);
 		}
 		//uint32_t profile_start = SysTick->CNT;
@@ -165,10 +128,6 @@ int main()
 
 		//memset( ssd1306_buffer, ((t+x+y)&1)?0xaa:0x55, sizeof( ssd1306_buffer ) );
 		t++;
-		//ssd1306_refresh();
-		ssd1306_cmd(SSD1306_COLUMNADDR);
-		ssd1306_cmd(SSD1306_OFFSET);   // Column start address (0 = reset)
-		ssd1306_cmd(SSD1306_OFFSET+SSD1306_W-1); // Column end address (127 = reset)
 
 		freeTime = nextFrame - SysTick->CNT;
 		while( (int32_t)(SysTick->CNT - nextFrame) < 0 );
@@ -178,14 +137,21 @@ int main()
 		// 533333 is 90Hz -- 90Hz seems about the max you can go with default 0xd5 settings.
 		// 400000 is 120Hz -- Only possible when cranking D5 at 0xF0 and 0xa8 == 0x31
 
-		ssd1306_pkt_send( (const uint8_t[]){0xD3, 0x32}, 2, 1 ); // Move the cursor "off screen"
-		ssd1306_pkt_send( (const uint8_t[]){0xA8, 0x01}, 2, 1 ); // Scan over two scanlines to hide updates
+		// Move the cursor "off screen"
+		// Scan over two scanlines to hide updates
+		ssd1306_pkt_send( 
+			(const uint8_t[]){0xD3, 0x32, 0xA8, 0x01,
+			// Column start address (0 = reset)
+			// Column end address (127 = reset)
+			SSD1306_COLUMNADDR, SSD1306_OFFSET, SSD1306_OFFSET+SSD1306_W-1 },
+			7, 1 );
 
 		// Send data
 		ssd1306_data(ssd1306_buffer, 64*48/8);
 
-		ssd1306_pkt_send( (const uint8_t[]){0xD3, 0x3e}, 2, 1 ); // Make it so it "would be" off screen but only by 2 pixels.
-		ssd1306_pkt_send( (const uint8_t[]){0xA8, 0x31}, 2, 1 ); // Overscan screen by 2 pixels, but release from 2-scanline mode.
+		// Make it so it "would be" off screen but only by 2 pixels.
+		// Overscan screen by 2 pixels, but release from 2-scanline mode.
+		ssd1306_pkt_send( (const uint8_t[]){0xD3, 0x3e, 0xA8, 0x31}, 4, 1 ); 
 	}
 }
 
