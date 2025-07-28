1efb2348db761afff37032debb64a00423612928
cnlohr <lohr85@gmail.com>
2025-07-28 05:05:15 -0700

    Bump code test

--- ch570/ch570ittyahrs/adctest/firmware.c
+++ ch570/ch570ittyahrs/adctest/firmware.c
@@ -1,4 +1,3 @@
-```c
 // ADC test on ch570.
 
 #include "ch32fun.h"
@@ -8,18 +7,47 @@
 // Timer Capture comes from Input (not that it matters) + Enable.
 //
 // Falling edge generates interrupt. (Disable interrupt for now)
-#define CMP_CONFIG 0b001111111 | (2 << 10)
+#define CMP_CONFIG ( 0x7 << 4 ) | 0b1111
+// | (2 << 10)
 
 int takeReading( void ) __attribute__((section(".srodata")));
 int takeReading( void ) 
 {
-	uint32_t r = SysTick->CNT;
 	funPinMode( PA7, GPIO_ModeIN_Floating );
 	// Time how long it takes to float up to 400mV.
-	while(!(*((vu8*)0x40001057)));
-	r = SysTick->CNT - r;
+	//while(!(*((vu8*)0x40001057)));
+	volatile uint8_t * vcheck = (uint8_t*)0x40001057;
+
+	int elapsed;
+	asm volatile("\n\
+		li a3, 0x00010000\n\
+		lw a4, 0(%[systickcnt])\n\
+		li a2, 0\n\
+.option   push;\n\
+.option   norvc;\n\
+rchk:\n\
+		bnez a2, rcdone\n\
+		lbu a2, 0(%[vcheck])\n\
+		bnez a2, rcdone\n\
+			addi a3, a3, -1\n\
+		lbu a2, 0(%[vcheck])\n\
+		bnez a2, rcdone\n\
+			bnez a3, rchk\n\
+		lbu a2, 0(%[vcheck])\n\
+		bnez a2, rcdone\n\
+		lbu a2, 0(%[vcheck])\n\
+		j rchk /* ???? Why can't I do this the other way around */ \n\
+.option   pop;\n\
+rcto: \n\
+		li %[r], -1\n\
+		j finish\n\
+rcdone:\n\
+		lw a3, 0(%[systickcnt])\n\
+		sub %[r], a3, a4\n\
+finish:\n\
+		" : [r]"=r"(elapsed) : [vcheck]"r"(vcheck), [systickcnt]"r"(&SysTick->CNT) : "a2", "a3", "a4"  );
 	funPinMode( PA7, GPIO_ModeOut_PP_5mA );
-	return r;
+	return elapsed;
 }
 
 int fullReading( void ) __attribute__((section(".srodata")));
@@ -31,11 +59,11 @@ int fullReading( void )
 	// Oversample like crazy.
 	int i;
 	uint32_t tot = 0;
-	for( i = 0; i < 256; i++ )
+	for( i = 0; i < 1; i++ )
 	{
 		tot += takeReading();
 	}
-	return tot>>4;
+	return tot;
 }
 
 int main()
@@ -53,4 +81,4 @@ int main()
 		printf( "%d\n", r );
 	}
 }
-```
+
