9fe449ba57b35e463637fbb9f31fc9a5ee43a15c
cnlohr <lohr85@gmail.com>
2025-07-30 01:43:28 -0700

    Bump technique using sleep

--- ch570/ch570ittyahrs/adctest/firmware.c
+++ ch570/ch570ittyahrs/adctest/firmware.c
@@ -1,84 +1,196 @@
-// ADC test on ch570.
+// ADC test on ch570...
+// 
+// Methodology:
+//  Connect a 0.1uF capcaitor between PA7 and GND
+//  Connect a 5.1k resistor between PA7 and the voltage you want to measure.
+//  WARNING: this will inject a little bit of noise on your signal.
+//
+//  Increasing resistance will improve reading quality, and reduce ripple, but take longer.
+//  Increasing capacitance will reduce ripple, take more power and take longer.
+//
+// NOTE: Most capacitors in the 0.1-10uF range are more than their listed value.
+// You should calibrate.
+// NOTE: You may use PA4, but will need to change the code.
 
 #include "ch32fun.h"
 #include <stdio.h>
 
-// VERF = 400mV and PA7 - CMP_VERF
+// VERF = 100mV and PA7 - CMP_VERF
 // Timer Capture comes from Input (not that it matters) + Enable.
 //
-// Falling edge generates interrupt. (Disable interrupt for now)
-#define CMP_CONFIG ( 0x7 << 4 ) | 0b1111
-// | (2 << 10)
+// Rising edge generates interrupt. (Disable interrupt for now)
+// After a lot of testing 100mV seems best.
+
+#define CMP_CONFIG ( 0x2 << 4 ) | 0b1111 | (0b00<<10)
+		#define OVERSAMPLE  8
+
+volatile unsigned switch_time;
+
+// Because we are only operating on pinned register-variables, we can
+// just straight up do a naked isr.
+void CMP_IRQHandler(void) __attribute__((interrupt)) __attribute__((section(".srodata")));
+void CMP_IRQHandler(void)
+{
+	switch_time = SysTick->CNT;
+	R32_CMP_CTRL = CMP_CONFIG; // Disable ISR
+}
+
+void SysTick_Handler(void) __attribute__((interrupt))  __attribute__((section(".srodata")));
+void SysTick_Handler(void)
+{
+	// Clear the trigger state for the next IRQ
+	SysTick->SR = 0x00000000;
+}
+
+void SmartDelayOrISR( int n )  __attribute__((section(".srodata")));
+void SmartDelayOrISR( int n ) 
+{
+#if !FUNCONF_SYSTICK_USE_HCLK
+	#error This function requires FUNCONF_SYSTICK_USE_HCLK
+#endif
+	if( n < 220 )
+	{
+		uint32_t targend = SysTick->CNTL + n;
+		while( ((int32_t)( SysTick->CNTL - targend )) < 0 );
+	}
+	else
+	{
+		SysTick->CMP = SysTick->CNT + n;
+		SysTick->CTLR = 0x07; // Enable Interrupt
+		__WFI();
+		SysTick->CTLR = 0x05; // Disable Interrupt
+	}
+}
 
 int takeReading( void ) __attribute__((section(".srodata")));
+
 int takeReading( void ) 
 {
+
+	volatile uint32_t start = SysTick->CNT;
+	switch_time = start;
+
+	// Enable ISR
+	R32_CMP_CTRL = CMP_CONFIG | (1<<8);
+
 	funPinMode( PA7, GPIO_ModeIN_Floating );
 	// Time how long it takes to float up to 400mV.
-	//while(!(*((vu8*)0x40001057)));
-	volatile uint8_t * vcheck = (uint8_t*)0x40001057;
 
-	int elapsed;
-	asm volatile("\n\
-		li a3, 0x00010000\n\
-		lw a4, 0(%[systickcnt])\n\
-		li a2, 0\n\
-.option   push;\n\
-.option   norvc;\n\
-rchk:\n\
-		bnez a2, rcdone\n\
-		lbu a2, 0(%[vcheck])\n\
-		bnez a2, rcdone\n\
-			addi a3, a3, -1\n\
-		lbu a2, 0(%[vcheck])\n\
-		bnez a2, rcdone\n\
-			bnez a3, rchk\n\
-		lbu a2, 0(%[vcheck])\n\
-		bnez a2, rcdone\n\
-		lbu a2, 0(%[vcheck])\n\
-		j rchk /* ???? Why can't I do this the other way around */ \n\
-.option   pop;\n\
-rcto: \n\
-		li %[r], -1\n\
-		j finish\n\
-rcdone:\n\
-		lw a3, 0(%[systickcnt])\n\
-		sub %[r], a3, a4\n\
-finish:\n\
-		" : [r]"=r"(elapsed) : [vcheck]"r"(vcheck), [systickcnt]"r"(&SysTick->CNT) : "a2", "a3", "a4"  );
-	funPinMode( PA7, GPIO_ModeOut_PP_5mA );
-	return elapsed;
+	SmartDelayOrISR( 100000 );
+
+	R32_CMP_CTRL = CMP_CONFIG; // Disable ISR
+
+	funPinMode( PA7, GPIO_ModeOut_PP_20mA );
+
+	return switch_time - start;
 }
 
 int fullReading( void ) __attribute__((section(".srodata")));
 int fullReading( void )
 {
-	// Get rid of any bias.  (This is required)
-	takeReading();
-
 	// Oversample like crazy.
 	int i;
+	takeReading();
 	uint32_t tot = 0;
-	for( i = 0; i < 1; i++ )
+	for( i = 0; i < OVERSAMPLE; i++ )
 	{
+		// Need 1us to reset ADC between samples.
+		Delay_Us(1);
 		tot += takeReading();
 	}
 	return tot;
 }
 
+
+void setupPA7ForADCRead()
+{
+	funPinMode( PA7, GPIO_ModeOut_PP_5mA );
+	NVIC_EnableIRQ(ADC_IRQn);
+	NVIC_EnableIRQ(SysTick_IRQn);
+	__enable_irq();
+}
+
 int main()
 {
-	int i;
 	SystemInit();
 
 	funGpioInitAll(); // no-op on ch5xx
 
-	R32_CMP_CTRL = CMP_CONFIG;
+
+	setupPA7ForADCRead();
 
 	while(1)
 	{
+		uint32_t start = SysTick->CNT;
 		int r = fullReading();
-		printf( "%d\n", r );
+		uint32_t end = SysTick->CNT;
+		r -= 8; // There is a delay before the release of the pin of about 8 cycles
+
+		// tau = R * C
+		// Vcomp = Vmeas(1 - 2.718 ^ (-t/tau))
+		// Vmeas = Vcomp / (1 - 2.718 ^ (-t/tau))
+		//
+		// For example:
+		//t = ticks / 60000000
+		//tau = 5.1k * 0.1uF
+		//Vcomp = 0.1V
+		//Vmeas = Vcomp / (1 - e ^ (-t/tau))
+		//
+		//Vcomp = 0.1
+		//t = 533 / 60000000
+		//tau = 0.0051
+		//-t/tau = -0.00174183
+		//e^that = 0.998259686
+
+		//A complete computation is = 0.1/(1-EXP(-ticks/60000000/(5100*0.0000001)))
+		// Which oddly works out to almost exactly: = 3060/ticks+0.05
+		// 3060=60000000*(5100*0.0000001)*0.1
+		//
+		// 3060000/ticks + 50 in millivolts.
+		#define CAPACITANCE 0.0000001167
+		#define RESISTANCE  5100.0
+		#define VREF 0.1
+		#define COEFFICIENT (const uint32_t)(FUNCONF_SYSTEM_CORE_CLOCK*(RESISTANCE*CAPACITANCE)*VREF*1000*OVERSAMPLE)
+
+		int vtot = COEFFICIENT/r + 100; //100mV offset (vComp)
+		printf( "%d %d %d\n", r, vtot, end-start );
 	}
 }
+
+
+#if 0
+// An example of doing the ADC read without ISRs
+int takeReadingNoISR( void ) __attribute__((section(".srodata")));
+int takeReadingNoISR( void )
+{
+	switch_time = SysTick->CNT;
+	volatile uint32_t start = switch_time;
+
+	// Enable ISR
+	R32_CMP_CTRL = CMP_CONFIG;
+
+	funPinMode( PA7, GPIO_ModeIN_Floating );
+	// Time how long it takes to float up to 400mV.
+
+	volatile uint8_t * v = (((vu8*)0x40001057));
+
+	int timeout = 100000;
+	do
+	{
+		if( *v ) break;
+		if( !timeout ) break;
+		asm volatile( "" : "=r"(timeout) );
+		if( *v ) break;
+		timeout--;
+	} while( !*v );
+	switch_time = SysTick->CNT;
+
+	// Make sure GCC don't do any jibber jabber.
+	//asm volatile( "" : "=r"(switch_time) : : );
+
+	funPinMode( PA7, GPIO_ModeOut_PP_5mA );
+
+	return switch_time - start;
+}
+#endif
 
--- ch570/ch570ittyahrs/adctest/funconfig.h
+++ ch570/ch570ittyahrs/adctest/funconfig.h
@@ -1,6 +1,8 @@
 #ifndef _FUNCONFIG_H
 #define _FUNCONFIG_H
 
+#define FUNCONF_ISR_IN_RAM        1
+
 #define FUNCONF_SYSTICK_USE_HCLK  1
 #define FUNCONF_USE_HSI           0 // CH5xx does not have HSI
 #define FUNCONF_USE_HSE           1
@@ -10,5 +12,6 @@
 #define FUNCONF_DEBUG_HARDFAULT   0
 #define FUNCONF_USE_CLK_SEC       0
 #define FUNCONF_INIT_ANALOG       0 // ADC is not implemented yet
+
 
 #endif
