12262c3213ab60a5422632e0fef1520248057529
cnlohr <lohr85@gmail.com>
2026-05-03 21:20:07 -0700

    Playing more with CRCs

--- ch32v006/usbpdtest/tablegen/crcfinder.c
+++ ch32v006/usbpdtest/tablegen/crcfinder.c
@@ -7,9 +7,9 @@
 //uint8_t example_packet[] = { 0xa6, 0x0b, 0x18, 0xd0, 0x56, 0x2e, };
 
 
-//uint8_t example_packet[] = { 0xa1, 0x75, 0x2c, 0x91, 0x01, 0x08, 0x2c, 0xd1, 0x02, 0x00, 0x2c, 0xc1, 0x03, 0x00, 0x2c, 0xb1, 0x04, 0x00, 0xf4, 0x41, 0x06, 0x00, 0x64, 0x21, 0xdc, 0xc0, 0x64, 0x21, 0x90, 0xc1, 0x23, 0xb3, 0xe7, 0x14, };
+uint8_t example_packet3[] = { 0xa1, 0x75, 0x2c, 0x91, 0x01, 0x08, 0x2c, 0xd1, 0x02, 0x00, 0x2c, 0xc1, 0x03, 0x00, 0x2c, 0xb1, 0x04, 0x00, 0xf4, 0x41, 0x06, 0x00, 0x64, 0x21, 0xdc, 0xc0, 0x64, 0x21, 0x90, 0xc1, 0x23, 0xb3, 0xe7, 0x14, };
 
-//uint8_t example_packet[] = { 0xa1, 0x73, 0x2c, 0x91, 0x01, 0x08, 0x2c, 0xd1, 0x02, 0x00, 0x2c, 0xc1, 0x03, 0x00, 0x2c, 0xb1, 0x04, 0x00, 0xf4, 0x41, 0x06, 0x00, 0x64, 0x21, 0xdc, 0xc0, 0x64, 0x21, 0x90, 0xc1, 0xba, 0x63, 0x86, 0x85, };
+uint8_t example_packet4[] = { 0xa1, 0x73, 0x2c, 0x91, 0x01, 0x08, 0x2c, 0xd1, 0x02, 0x00, 0x2c, 0xc1, 0x03, 0x00, 0x2c, 0xb1, 0x04, 0x00, 0xf4, 0x41, 0x06, 0x00, 0x64, 0x21, 0xdc, 0xc0, 0x64, 0x21, 0x90, 0xc1, 0xba, 0x63, 0x86, 0x85, };
 
 //0xea4afa09
 /*
@@ -25,33 +25,274 @@ const char example_packet[] = {
 
 int main()
 {
-	uint64_t checkzero = 0xffffffff;
-//	for( checkzero = 0; checkzero < 0x100000000ULL; checkzero++ )
+	int symbol;
+
+	// Forwards
+
+	for( symbol = 0; symbol < 16; symbol++ )
 	{
-		uint32_t current_crc = 0xffffffff;
 
-		if( ( checkzero & 0xffffff) == 0 ) printf( "%08lx\n", checkzero );
-
+		uint32_t USE_POLY = CRC_POLY;
+/*
 		int i;
-		for( i = 0; i < sizeof(example_packet); i++ )
+		for( i = 0; i < 32; i++ )
 		{
-			uint8_t by = example_packet[i];
+			if( ( CRC_POLY >> i ) & 1 ) USE_POLY |= (1ULL<<(31-i));
+		}
+*/
+		uint32_t current_crc = 0;
+
+		{
+			uint8_t by = symbol;
+
 			uint32_t newbit, newword;
 			uint32_t rl_crc;
-			for(int i=0; i<8; i++) {
-				newbit = ((current_crc>>31) ^ ((by>>i)&1)) & 1;
-				if(newbit) newword=CRC_POLY; else newword=0;
+			for(int i=0; i<4; i++) {
+				newbit = ((current_crc>>31) ^ ((by>>(3-i))&1)) & 1;
+				if(newbit) newword=USE_POLY-1; else newword=0;
 				rl_crc = (current_crc<<1) | newbit;
 				current_crc = rl_crc ^ newword;
 			}
 		}
-		printf( "%08x\n", current_crc );
+		printf( "0x%08x, ", current_crc );
+	}
+	printf( "\n" );
+
+	{
+		uint32_t USE_POLY = CRC_POLY;
+		uint32_t start_crc = 0x10000000;
+		uint32_t current_crc = start_crc;
+		uint8_t bytest1 = 1;
+		uint8_t bytest2 = 1;
+
+		{
+			uint8_t by1 = bytest1;
+			uint8_t by2 = bytest2;
+
+			uint32_t newbit, newword;
+			uint32_t rl_crc;
+			for(int i=0; i<4; i++) {
+				newbit = ((current_crc>>31) ^ ((by1>>i)&1)) & 1;
+				if(newbit) newword=USE_POLY-1; else newword=0;
+				rl_crc = (current_crc<<1) | newbit;
+				current_crc = rl_crc ^ newword;
+			}
+		printf( "0x%08x\n", current_crc );
+
+/*
+			for(int i=0; i<4; i++) {
+				newbit = ((current_crc>>31) ^ ((by2>>i)&1)) & 1;
+				if(newbit) newword=USE_POLY-1; else newword=0;
+				rl_crc = (current_crc<<1) | newbit;
+				current_crc = rl_crc ^ newword;
+			}
+		printf( "0x%08x\n", current_crc );
+*/
+		}
+
+//0x04C11DB7
+
+
+		uint32_t test_poly[] = { 0x00000000, 0x2608edb8, 0x130476dc, 0x350c9b64, 0x09823b6e, 0x2f8ad6d6, 0x1a864db2, 0x3c8ea00a, 0x04c11db7, 0x22c9f00f, 0x17c56b6b, 0x31cd86d3, 0x0d4326d9, 0x2b4bcb61, 0x1e475005, 0x384fbdbd, };
+		//uint32_t test_poly_transpose[] = { 0x00000000, 0x04c11db7, 0x09823b6e, 0x0d4326d9, 0x130476dc, 0x17c56b6b, 0x1a864db2, 0x1e475005, 0x2608edb8, 0x22c9f00f, 0x2f8ad6d6, 0x2b4bcb61, 0x350c9b64, 0x31cd86d3, 0x3c8ea00a, 0x384fbdbd, };
+
+		USE_POLY = CRC_POLY;
+		current_crc = start_crc;
+
+		{
+			uint8_t by1 = bytest1;
+			uint8_t by2 = bytest2;
+printf( "STARTING: %08x %08x\n", by1, current_crc>>28 );
+			current_crc = (test_poly[(by1) & 0xf]) ^ (current_crc<<4);
+		printf( "0x%08x\n", current_crc );
+//			current_crc = (test_poly[(by2) & 0xf]) ^ (current_crc<<4);
+///		printf( "0x%08x\n", current_crc );
+
+		}
+
+
+
+	}
+
+
+
+return 0;
+
+
+		int i;
+
+
+	uint64_t checkzero = 
+		//0;
+		0xffffffff;
+		//0xb877fcd2;
+	// b877fcd2
+	//for( checkzero = 0; checkzero < 0x100000000ULL; checkzero++ )
+	{
+		uint32_t current_crc = checkzero;
+
+//		if( ( checkzero & 0xffffff) == 0 ) printf( "%08lx\n", checkzero );
+#if 0
+		for( i = 0; i < sizeof(example_packet4); i++ )
+		{
+			uint8_t by = example_packet4[i];
+
+			uint32_t newbit, newword;
+			uint32_t rl_crc;
+			for(int i=0; i<8; i++) {
+				newbit = ((current_crc>>31) ^ ((by>>i)&1)) & 1;
+				if(newbit) newword=CRC_POLY-1; else newword=0;
+				rl_crc = (current_crc<<1) | newbit;
+				current_crc = rl_crc ^ newword;
+			}
+		}
+#endif
+static const uint32_t crc32_posix_lut[16] = {
+//0x00000000,0x2608edb8,0x130476dc,0x350c9b64,0x09823b6e,0x2f8ad6d6,0x1a864db2,0x3c8ea00a,0x04c11db7, 0x22c9f00f,0x17c56b6b,0x31cd86d3,0x0d4326d9,0x2b4bcb61,0x1e475005,0x384fbdbd,
+0x00000000,0x04C11DB7,0x09823B6E,0x0D4326D9,0x130476DC,0x17C56B6B,0x1A864DB2,0x1E475005, 0x2608EDB8,0x22C9F00F,0x2F8AD6D6,0x2B4BCB61,0x350C9B64,0x31CD86D3,0x3C8EA00A,0x384FBDBD
+//0x00000000,0x1DB71064,0x3B6E20C8,0x26D930AC,0x76DC4190,0x6B6B51F4,0x4DB26158,0x5005713C,    0xEDB88320,0xF00F9344,0xD6D6A3E8,0xCB61B38C,0x9B64C2B0,0x86D3D2D4,0xA00AE278,0xBDBDF21C
+
+};
+
+static const uint32_t my_flip_lut[16] = {
+//0x00000000, 0xdb261488, 0x6d930a44, 0xb6b51ecc, 0x36c98522, 0xedef91aa, 0x5b5a8f66, 0x807c9bee, 0xedb8831e, 0x369e9796, 0x802b895a, 0x5b0d9dd2, 0xdb71063c, 0x005712b4, 0xb6e20c78, 0x6dc418f0,  // Flipped bit ordering.
+0x00000000,0x2608edb8,0x130476dc,0x350c9b64,0x09823b6e,0x2f8ad6d6,0x1a864db2,0x3c8ea00a,0x04c11db7, 0x22c9f00f,0x17c56b6b,0x31cd86d3,0x0d4326d9,0x2b4bcb61,0x1e475005,0x384fbdbd, // Originaly poly. Not flipped.
+//0x00000000,0x04C11DB7,0x09823B6E,0x0D4326D9,0x130476DC,0x17C56B6B,0x1A864DB2,0x1E475005, 0x2608EDB8,0x22C9F00F,0x2F8AD6D6,0x2B4BCB61,0x350C9B64,0x31CD86D3,0x3C8EA00A,0x384FBDBD
+//0x00000000,0x1DB71064,0x3B6E20C8,0x26D930AC,0x76DC4190,0x6B6B51F4,0x4DB26158,0x5005713C,    0xEDB88320,0xF00F9344,0xD6D6A3E8,0xCB61B38C,0x9B64C2B0,0x86D3D2D4,0xA00AE278,0xBDBDF21C
+};
+
+
+
+  static const uint32_t crctab[256] = {
+    0x00000000, 0x04c11db7, 0x09823b6e, 0x0d4326d9,
+    0x130476dc, 0x17c56b6b, 0x1a864db2, 0x1e475005,
+    0x2608edb8, 0x22c9f00f, 0x2f8ad6d6, 0x2b4bcb61,
+    0x350c9b64, 0x31cd86d3, 0x3c8ea00a, 0x384fbdbd,
+    0x4c11db70, 0x48d0c6c7, 0x4593e01e, 0x4152fda9,
+    0x5f15adac, 0x5bd4b01b, 0x569796c2, 0x52568b75,
+    0x6a1936c8, 0x6ed82b7f, 0x639b0da6, 0x675a1011,
+    0x791d4014, 0x7ddc5da3, 0x709f7b7a, 0x745e66cd,
+    0x9823b6e0, 0x9ce2ab57, 0x91a18d8e, 0x95609039,
+    0x8b27c03c, 0x8fe6dd8b, 0x82a5fb52, 0x8664e6e5,
+    0xbe2b5b58, 0xbaea46ef, 0xb7a96036, 0xb3687d81,
+    0xad2f2d84, 0xa9ee3033, 0xa4ad16ea, 0xa06c0b5d,
+    0xd4326d90, 0xd0f37027, 0xddb056fe, 0xd9714b49,
+    0xc7361b4c, 0xc3f706fb, 0xceb42022, 0xca753d95,
+    0xf23a8028, 0xf6fb9d9f, 0xfbb8bb46, 0xff79a6f1,
+    0xe13ef6f4, 0xe5ffeb43, 0xe8bccd9a, 0xec7dd02d,
+    0x34867077, 0x30476dc0, 0x3d044b19, 0x39c556ae,
+    0x278206ab, 0x23431b1c, 0x2e003dc5, 0x2ac12072,
+    0x128e9dcf, 0x164f8078, 0x1b0ca6a1, 0x1fcdbb16,
+    0x018aeb13, 0x054bf6a4, 0x0808d07d, 0x0cc9cdca,
+    0x7897ab07, 0x7c56b6b0, 0x71159069, 0x75d48dde,
+    0x6b93dddb, 0x6f52c06c, 0x6211e6b5, 0x66d0fb02,
+    0x5e9f46bf, 0x5a5e5b08, 0x571d7dd1, 0x53dc6066,
+    0x4d9b3063, 0x495a2dd4, 0x44190b0d, 0x40d816ba,
+    0xaca5c697, 0xa864db20, 0xa527fdf9, 0xa1e6e04e,
+    0xbfa1b04b, 0xbb60adfc, 0xb6238b25, 0xb2e29692,
+    0x8aad2b2f, 0x8e6c3698, 0x832f1041, 0x87ee0df6,
+    0x99a95df3, 0x9d684044, 0x902b669d, 0x94ea7b2a,
+    0xe0b41de7, 0xe4750050, 0xe9362689, 0xedf73b3e,
+    0xf3b06b3b, 0xf771768c, 0xfa325055, 0xfef34de2,
+    0xc6bcf05f, 0xc27dede8, 0xcf3ecb31, 0xcbffd686,
+    0xd5b88683, 0xd1799b34, 0xdc3abded, 0xd8fba05a,
+    0x690ce0ee, 0x6dcdfd59, 0x608edb80, 0x644fc637,
+    0x7a089632, 0x7ec98b85, 0x738aad5c, 0x774bb0eb,
+    0x4f040d56, 0x4bc510e1, 0x46863638, 0x42472b8f,
+    0x5c007b8a, 0x58c1663d, 0x558240e4, 0x51435d53,
+    0x251d3b9e, 0x21dc2629, 0x2c9f00f0, 0x285e1d47,
+    0x36194d42, 0x32d850f5, 0x3f9b762c, 0x3b5a6b9b,
+    0x0315d626, 0x07d4cb91, 0x0a97ed48, 0x0e56f0ff,
+    0x1011a0fa, 0x14d0bd4d, 0x19939b94, 0x1d528623,
+    0xf12f560e, 0xf5ee4bb9, 0xf8ad6d60, 0xfc6c70d7,
+    0xe22b20d2, 0xe6ea3d65, 0xeba91bbc, 0xef68060b,
+    0xd727bbb6, 0xd3e6a601, 0xdea580d8, 0xda649d6f,
+    0xc423cd6a, 0xc0e2d0dd, 0xcda1f604, 0xc960ebb3,
+    0xbd3e8d7e, 0xb9ff90c9, 0xb4bcb610, 0xb07daba7,
+    0xae3afba2, 0xaafbe615, 0xa7b8c0cc, 0xa379dd7b,
+    0x9b3660c6, 0x9ff77d71, 0x92b45ba8, 0x9675461f,
+    0x8832161a, 0x8cf30bad, 0x81b02d74, 0x857130c3,
+    0x5d8a9099, 0x594b8d2e, 0x5408abf7, 0x50c9b640,
+    0x4e8ee645, 0x4a4ffbf2, 0x470cdd2b, 0x43cdc09c,
+    0x7b827d21, 0x7f436096, 0x7200464f, 0x76c15bf8,
+    0x68860bfd, 0x6c47164a, 0x61043093, 0x65c52d24,
+    0x119b4be9, 0x155a565e, 0x18197087, 0x1cd86d30,
+    0x029f3d35, 0x065e2082, 0x0b1d065b, 0x0fdc1bec,
+    0x3793a651, 0x3352bbe6, 0x3e119d3f, 0x3ad08088,
+    0x2497d08d, 0x2056cd3a, 0x2d15ebe3, 0x29d4f654,
+    0xc5a92679, 0xc1683bce, 0xcc2b1d17, 0xc8ea00a0,
+    0xd6ad50a5, 0xd26c4d12, 0xdf2f6bcb, 0xdbee767c,
+    0xe3a1cbc1, 0xe760d676, 0xea23f0af, 0xeee2ed18,
+    0xf0a5bd1d, 0xf464a0aa, 0xf9278673, 0xfde69bc4,
+    0x89b8fd09, 0x8d79e0be, 0x803ac667, 0x84fbdbd0,
+    0x9abc8bd5, 0x9e7d9662, 0x933eb0bb, 0x97ffad0c,
+    0xafb010b1, 0xab710d06, 0xa6322bdf, 0xa2f33668,
+    0xbcb4666d, 0xb8757bda, 0xb5365d03, 0xb1f740b4,
+  };
+  
+  #define M1 0xffffff
+  #define M2 0xffffff00
+		for( i = 0; i < sizeof(example_packet4); i++ )
+		{
+			uint32_t data = example_packet4[i];
+			//uint8_t combo = ( current_crc >> 24 ) ^ data;
+
+///////////////////////////////////// Check but wrong way:
+
+//     current_crc=(current_crc<<8)^crctab[((current_crc>>24)&0xff)^data];
+// is equivelent to
+//  #define M2a 0xfffffff0
+//     current_crc=((current_crc<<4))^crc32_posix_lut[(((current_crc>>28))^(data>>4))&0xf];
+//     current_crc=((current_crc<<4))^crc32_posix_lut[(((current_crc>>28))^(data))&0xf];
+//0x00000000,0x04C11DB7,0x09823B6E,0x0D4326D9,0x130476DC,0x17C56B6B,0x1A864DB2,0x1E475005, 0x2608EDB8,0x22C9F00F,0x2F8AD6D6,0x2B4BCB61,0x350C9B64,0x31CD86D3,0x3C8EA00A,0x384FBDBD
+
+/* someone's note
+
+    Normal:  crc = table[ ((crc>>24) ^ *data++) & 0xFF ] ^ (crc << 8);  << this is what's used now.
+    Reflect: crc = table[ (crc       ^ *data++) & 0xFF ] ^ (crc >> 8); << How switch to this?
+*/
+
+// reflect
+//     current_crc=((current_crc>>4))^my_flip_lut[((current_crc)^(data   ))&0xf];
+//     current_crc=((current_crc>>4))^my_flip_lut[((current_crc)^(data>>4))&0xf];
+
+// normal
+     current_crc=((current_crc<<4))^my_flip_lut[((current_crc>>28)^(data   ))&0xf];
+     current_crc=((current_crc<<4))^my_flip_lut[((current_crc>>28)^(data>>4))&0xf];
+
+  //  current_crc = crc32_posix_lut[(current_crc ^  data      ) & 0x0F] ^ (current_crc >> 4);
+  //   current_crc = crc32_posix_lut[(current_crc ^ (data >> 4)) & 0x0F] ^ (current_crc >> 4);
+
+//	current_crc = ( current_crc << 4 ) ^ crc32_posix_lut[((current_crc>>28)^data)&0xf]; data >>= 4;
+//	current_crc = ( current_crc << 4 ) ^ crc32_posix_lut[((current_crc>>28)^data)&0xf];
+
+
+//			current_crc ^= (uint32_t)data;
+//			current_crc = (current_crc >> 4) ^ crc32_posix_lut[current_crc & 0xf];
+//			current_crc = (current_crc >> 4) ^ crc32_posix_lut[current_crc & 0xf];
+
+
+//current_crc ^= (uint32_t)data << 24;
+//current_crc = (current_crc << 4) ^ crc32_posix_lut[current_crc >> 28];
+//current_crc = (current_crc << 4) ^ crc32_posix_lut[current_crc >> 28];
+
+//			current_crc = crc32_posix_lut[(current_crc ^ data) & 0x0F] ^ (current_crc >> 4);
+//			current_crc = crc32_posix_lut[(current_crc ^ (data >> 4)) & 0x0F] ^ (current_crc >> 4);
+//			current_crc = crc32_posix_lut[((current_crc>>28) ^ data) & 0x0F] ^ (current_crc);
+//			current_crc = crc32_posix_lut[((current_crc>>28) ^ (data >> 4)) & 0x0F] ^ (current_crc);
+
+		}
+		printf( "\n" );
+
+		//Target c704dd7b
+
+		printf( "Out: %08x\n", current_crc );
 
 		if( current_crc == 0 )
 		{
 			printf( "Initial: %08lx\n", checkzero );
 		}
-
+#if 0
 		int crccheck = 0;
 		uint32_t j, bit;
 		current_crc = ~current_crc;
@@ -61,5 +302,6 @@ int main()
 		    crccheck |= bit<<j;
 		}
 		printf( "%08x %08x\n", crccheck, current_crc );
+#endif
 	}
 }
