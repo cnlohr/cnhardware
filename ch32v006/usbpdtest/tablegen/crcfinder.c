a86b32bd71a2ba62dc25d6cff21c5aff765c39ab
cnlohr <lohr85@gmail.com>
2026-05-06 01:45:03 -0700

    Table based almost working

--- ch32v006/usbpdtest/tablegen/crcfinder.c
+++ ch32v006/usbpdtest/tablegen/crcfinder.c
@@ -23,6 +23,10 @@ const char example_packet[] = {
  };
 */
 
+uint32_t ct[] = {
+0x00000000, 0x04c11db7, 0x09823b6e, 0x0d4326d9, 0x130476dc, 0x17c56b6b, 0x1a864db2, 0x1e475005, 0x2608edb8, 0x22c9f00f, 0x2f8ad6d6, 0x2b4bcb61, 0x350c9b64, 0x31cd86d3, 0x3c8ea00a, 0x384fbdbd,
+ };
+
 int main()
 {
 	int symbol;
@@ -60,9 +64,9 @@ int main()
 
 	{
 		uint32_t USE_POLY = CRC_POLY;
-		uint32_t start_crc = 0x10000000;
+		uint32_t start_crc = 0x22400002;
 		uint32_t current_crc = start_crc;
-		uint8_t bytest1 = 1;
+		uint8_t bytest1 = 5;
 		uint8_t bytest2 = 1;
 
 		{
@@ -77,7 +81,17 @@ int main()
 				rl_crc = (current_crc<<1) | newbit;
 				current_crc = rl_crc ^ newword;
 			}
-		printf( "0x%08x\n", current_crc );
+		printf( "Compare: 0x%08x\n", current_crc );
+
+		uint32_t mycrc = start_crc;
+		 by1 = bytest1;
+
+		// Reverse by1 bits.
+		uint8_t rev[] = { 0b0000, 0b1000, 0b0100, 0b1100, 0b0010, 0b1010, 0b0110, 0b1110, 0b0001, 0b1001, 0b0101, 0b1101, 0b0011, 0b1011, 0b0111, 0b1111 };
+
+		mycrc = ( ( mycrc << 4 ) ) ^ ct[((mycrc >> 28) ^ rev[by1] )];
+
+		printf( "Compare: 0x%08x\n", mycrc );
 
 /*
 			for(int i=0; i<4; i++) {
