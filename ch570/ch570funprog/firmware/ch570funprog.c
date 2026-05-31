c7a48c527182926e5fe9ed3e591f6a5609cdfef0
cnlohr <lohr85@gmail.com>
2026-05-31 01:08:14 -0700

    Poking at ch570 work

--- ch570/ch570funprog/firmware/ch570funprog.c
+++ ch570/ch570funprog/firmware/ch570funprog.c
@@ -1,126 +1,123 @@
-#include <stdio.h>
 #include "ch32fun.h"
+#include <stdio.h>
+#include <string.h>
 #include "fsusb.h"
 
-#define POWER_IO    PA7
-#define SWC_IO      PA3
-#define SWD_IO      PA2
-#define USB_DATA_BUF_SIZE 256
+#define LED PA7
+#define LED_ON 0
 
-__attribute__((aligned(4))) static volatile uint8_t gs_usb_data_buf[USB_DATA_BUF_SIZE];
+uint32_t count;
+int doreboot = 0;
+int last = 0;
 
-/*
-__HIGH_CODE
-void blink(int n) {
-	for(int i = n-1; i >= 0; i--) {
-		funDigitalWrite( LED, FUN_LOW ); // Turn on LED
-		Delay_Ms(33);
-		funDigitalWrite( LED, FUN_HIGH ); // Turn off LED
-		if(i) Delay_Ms(33);
-	}
+void handle_debug_input( int numbytes, uint8_t * data )
+{
+	last = data[0];
+	count += numbytes;
 }
-*/
 
-int HandleSetupCustom( struct _USBState * ctx, int setup_code) {
+int lrx = 0;
+uint8_t scratchpad[256];
+
+int HandleHidUserSetReportSetup( struct _USBState * ctx, tusb_control_request_t * req )
+{
+	int id = req->wValue & 0xff;
+	if( id == 0xaa && req->wLength <= sizeof(scratchpad) )
+	{
+		ctx->pCtrlPayloadPtr = scratchpad;
+		lrx = req->wLength;
+		return req->wLength;
+	}
 	return 0;
 }
 
-int HandleInRequest( struct _USBState * ctx, int endp, uint8_t * data, int len ) {
+int HandleHidUserGetReportSetup( struct _USBState * ctx, tusb_control_request_t * req )
+{
+	int id = req->wValue & 0xff;
+	switch( id )
+	{
+	case 0xaa:
+	{
+		ctx->pCtrlPayloadPtr = scratchpad;
+		if( sizeof(scratchpad) - 1 < lrx )
+			return sizeof(scratchpad) - 1;
+		else
+			return lrx;
+	}
+	case 0xe2: // Copy the printf debug buffer out of DMDATA0.
+		memcpy( scratchpad, (char*)DMDATA0, 8 );
+		ctx->pCtrlPayloadPtr = scratchpad;
+		*DMDATA0 = 0;
+		return 8;
+	}
 	return 0;
 }
 
-__HIGH_CODE
-void HandleDataOut( struct _USBState * ctx, int endp, uint8_t * data, int len ) {
-	// this is actually the data rx handler
-	if ( endp == 0 ) {
-		// this is in the hsusb.c default handler
-		ctx->USBFS_SetupReqLen = 0; // To ACK
-	}
-	else if( endp == USB_EP_RX ) {
-		if(len == 4 && ((uint32_t*)data)[0] == 0x010001a2) {
-			USBFSReset();
-			//blink(2);
-			jump_isprom();
-		}
-		else if( gs_usb_data_buf[0] == 0 ) {
-			gs_usb_data_buf[0] = len;
-			for(int i = 0; i < len; i++) {
-				gs_usb_data_buf[i +1] = data[i];
+void HandleHidUserReportDataOut( struct _USBState * ctx, uint8_t * data, int len )
+{
+	switch( data[0] )
+	{
+	case 0xe1:
+		if( len > 7 )
+		{
+			if( strncmp( (char*)(data+1), "\xbe\xef\x00\xc0\x01\xd0\x0d", 7 ) == 0 )
+			{
+				doreboot = 1000;
 			}
 		}
-		else {
-			// previous usb buffer not consumed yet, what should we do?
-		}
-
-		ctx->USBFS_Endp_Busy[USB_EP_RX] = 0;
+		break;
 	}
 }
 
-void Tick()
+int HandleHidUserReportDataIn( struct _USBState * ctx, uint8_t * data, int len )
 {
-	//funDigitalWrite( SWD_IO, FUN_HIGH ); // Turn off LED
-	//funDigitalWrite( SWD_IO, FUN_LOW ); // Turn on LED
+	return len;
 }
 
-__HIGH_CODE
-void ASetup()
+void HandleHidUserReportOutComplete( struct _USBState * ctx )
 {
-	SYS_SAFE_ACCESS( R8_PWM_CONFIG = (2 << 1) | 0xff; ); // 64-cycle
-	SYS_SAFE_ACCESS( R8_SLP_CLK_OFF1=2; ); // disable AES
-	SYS_SAFE_ACCESS( R8_SLP_CLK_OFF0=(1<<4); ); // disable uart
-	SYS_SAFE_ACCESS( R16_PWM_CLOCK_DIV = 4; );
-
-	SYS_SAFE_ACCESS( R8_PWM_POLAR = RB_PWM3_POLAR; );
-	//R8_PWM_CONFIG = 0b100 | RB_PWM_CYCLE_SEL;
-
-	*((volatile uint16_t *)((&R8_PWM2_DATA))) = 12;
-	*((volatile uint16_t *)((&R8_PWM3_DATA))) = 12;
-
-	*((volatile uint16_t *)((&R16_PWM2_DATA))) = 12;
-	*((volatile uint16_t *)((&R16_PWM3_DATA))) = 12;
-
-	R8_PWM_OUT_EN = RB_PWM2_OUT_EN | RB_PWM3_OUT_EN;
-	R8_PWM_CONFIG |= RB_PWM_SYNC_EN;
-	R8_PWM_CONFIG |= RB_PWM_SYNC_START;
-
+	return;
 }
 
-int main() {
+__USBFS_FUN_ATTRIBUTE
+static __attribute__((noreturn)) void processLoop()
+{
+	while(1)
+	{
+		//printf( "%lu %08lx %lu %d %d\n", USBDEBUG0, USBDEBUG1, USBDEBUG2, 0, 0 );
+		if( doreboot )
+		{
+			if( --doreboot == 0 )
+			{
+#if defined(CH5xx)
+				USBFSReset();
+				jump_isprom();
+#else
+				// There aren't any other chips that can reboot into USB bootloader, are there?
+#endif
+			}
+		}
+		printf( "loop\n" );
+	}
+}
+
+int main()
+{
 	SystemInit();
 
-	funGpioInitAll(); // no-op on ch5xx
+	funGpioInitAll();
 
-	//HSECFG_Capacitance(HSECap_10p);
+	funPinMode( LED, GPIO_CFGLR_OUT_10Mhz_PP );
+	funDigitalWrite( LED, !LED_ON );
 
-//	funPinMode( SWC_IO, GPIO_CFGLR_OUT_10Mhz_PP ); // GPIO_CFGLR_IN_PU
-//	funPinMode( SWD_IO, GPIO_CFGLR_OUT_10Mhz_PP );
+	printf("USBFS starting...");
 
-//	USBFSSetup();
+	USBFSSetup();
 
-//	R32_PA_PD_DRV = SWD_IO | SWC_IO; // 20mA
-	R32_PA_DIR    = SWC_IO | SWD_IO | POWER_IO; // Drive out.
+	printf("ok\n");
 
-//	R16_PIN_ALTERNATE_H = 1; // Map UART off of port.
+	funDigitalWrite( LED, LED_ON );
 
-	ASetup();
-	// Remap
-printf( "%04x %04x %04x %04x %04x\n", R8_PWM_OUT_EN, R8_PWM_CONFIG, R16_PWM_CLOCK_DIV, R8_PWM3_DATA, R8_SLP_CLK_OFF1 );
-
-	funDigitalWrite( POWER_IO, FUN_LOW ); // Turn on output.
-while(1) {
-	//funDigitalWrite( SWD_IO, FUN_LOW );
-	//funDigitalWrite( SWD_IO, FUN_HIGH );
+	processLoop();
 }
 
-	while(1) {
-
-		Tick();
-
-		if( gs_usb_data_buf[0] ) {
-			// Send data back to PC.
-			while( USBFSCTX.USBFS_Endp_Busy[USB_EP_TX] & 1 );
-			USBFS_SendEndpointNEW( USB_EP_TX, (uint8_t*)&gs_usb_data_buf[1], gs_usb_data_buf[0], /*copy=*/1 ); // USBFS needs a copy here
-			gs_usb_data_buf[0] = 0;
-		}
-	}
-}
--- ch570/ch570funprog/firmware/usb_config.h
+++ ch570/ch570funprog/firmware/usb_config.h
@@ -4,44 +4,75 @@
 #include "funconfig.h"
 #include "ch32fun.h"
 
-#define FUSB_CONFIG_EPS       3 // Include EP0 in this count
+#define FUSB_CONFIG_EPS       2 // Include EP0 in this count
 #define FUSB_EP1_MODE         1 // TX (IN)
 #define FUSB_EP2_MODE         -1 // RX (OUT)
 #define USB_EP_TX             1
 #define USB_EP_RX             2
 #define FUSB_SUPPORTS_SLEEP   0
 #define FUSB_IO_PROFILE       0
+
+#define FUSB_HID_INTERFACES   2
+#define FUSB_CURSED_TURBO_DMA 0 // Hacky, but seems fine, shaves 2.5us off filling 64-byte buffers.
+#define FUSB_HID_USER_REPORTS 1
+#define FUSB_IO_PROFILE       0
 #define FUSB_USE_HPE          FUNCONF_ENABLE_HPE
-#define FUSB_EP_SIZE          64
-#define FUSB_SPEED            USB_SPEED_FULL
-#define FUSB_USER_HANDLERS    1 // To enable HandleDataOut
-#define FUSB_OUT_FLOW_CONTROL 0 // 0: auto ack
+#define FUSB_USER_HANDLERS    0
+#define FUSB_USE_DMA7_COPY    0
+#define FUSB_VDD_5V           FUNCONF_USE_5V_VDD
+#define FUSB_FROM_RAM         0
 
 #include "usb_defines.h"
 
-#define FUSB_USB_VID          0x1209
-#define FUSB_USB_PID          0xd035
-#define FUSB_USB_REV          0x0007
-#define FUSB_STR_MANUFACTURER u"ch570funprog"
-#define FUSB_STR_PRODUCT      u"Test"
-#define FUSB_STR_SERIAL       u"-test-"
+#define FUSB_USB_VID 0x1209
+#define FUSB_USB_PID 0xd035
+#define FUSB_USB_REV 0x0007
+#define FUSB_STR_MANUFACTURER u"ch32fun"
+#define FUSB_STR_PRODUCT      u"HID example device"
+#define FUSB_STR_SERIAL       u"007"
 
 //Taken from http://www.usbmadesimple.co.uk/ums_ms_desc_dev.htm
 static const uint8_t device_descriptor[] = {
-	18, //Length
-	1,  //Type (Device)
-	0x00, 0x02, //Spec
-	0x0, //Device Class
-	0x0, //Device Subclass
-	0x0, //Device Protocol  (000 = use config descriptor)
-	64, //Max packet size for EP0
+	18, //bLength - Length of this descriptor
+	1,  //bDescriptorType - Type (Device)
+	0x00, 0x02, //bcdUSB - The highest USB spec version this device supports (USB1.1)
+	0x0, //bDeviceClass - Device Class
+	0x0, //bDeviceSubClass - Device Subclass
+	0x0, //bDeviceProtocol - Device Protocol  (000 = use config descriptor)
+	64, //bMaxPacketSize - Max packet size for EP0
 	(uint8_t)(FUSB_USB_VID), (uint8_t)(FUSB_USB_VID >> 8), //idVendor - ID Vendor
 	(uint8_t)(FUSB_USB_PID), (uint8_t)(FUSB_USB_PID >> 8), //idProduct - ID Product
 	(uint8_t)(FUSB_USB_REV), (uint8_t)(FUSB_USB_REV >> 8), //bcdDevice - Device Release Number
-	1, //Manufacturer string
-	2, //Product string
-	3, //Serial string
-	1, //Max number of configurations
+	1, //iManufacturer - Index of Manufacturer string
+	2, //iProduct - Index of Product string
+	3, //iSerialNumber - Index of Serial string
+	1, //bNumConfigurations - Max number of configurations (if more then 1, you can switch between them)
+};
+
+
+static const uint8_t HIDAPIRepDesc[ ] =
+{
+	HID_USAGE_PAGE ( 0xff ), // Vendor-defined page.
+	HID_USAGE      ( 0x00 ),
+	HID_REPORT_SIZE ( 64 ),
+	HID_COLLECTION ( HID_COLLECTION_LOGICAL ),
+		HID_REPORT_COUNT   ( 254 ),
+		HID_REPORT_ID      ( 0xaa )
+		HID_USAGE          ( 0x01 ),
+		HID_FEATURE        ( HID_DATA | HID_VARIABLE | HID_ABSOLUTE ) ,
+		HID_REPORT_COUNT   ( 63 ), // For use with `hidapitester --vidpid 1209/D003 --open --read-feature 171`
+		HID_REPORT_ID      ( 0xab )
+		HID_USAGE          ( 0x01 ),	
+		HID_FEATURE        ( HID_DATA | HID_VARIABLE | HID_ABSOLUTE ) ,
+		HID_REPORT_COUNT   ( 8 ), // For use with `hidapitester --vidpid 1209/D003 --open --read-feature 171`
+		HID_REPORT_ID      ( 0xe2 )
+		HID_USAGE          ( 0x01 ),	
+		HID_FEATURE        ( HID_DATA | HID_VARIABLE | HID_ABSOLUTE ) ,
+		HID_REPORT_COUNT   ( 8 ), // For use with `hidapitester --vidpid 1209/D003 --open --read-feature 171`
+		HID_REPORT_ID      ( 0xe1 )
+		HID_USAGE          ( 0x01 ),	
+		HID_FEATURE        ( HID_DATA | HID_VARIABLE | HID_ABSOLUTE ) ,
+	HID_COLLECTION_END,
 };
 
 /* Configuration Descriptor Set */
@@ -50,39 +81,40 @@ static const uint8_t config_descriptor[ ] =
     /* Configuration Descriptor */
     0x09,                                                   // bLength
     0x02,                                                   // bDescriptorType
-    0x20, 0x00,                                             // wTotalLength
+    0x22, 0x00,                                             // wTotalLength
     0x01,                                                   // bNumInterfaces (1)
     0x01,                                                   // bConfigurationValue
     0x00,                                                   // iConfiguration
     0xA0,                                                   // bmAttributes: Bus Powered; Remote Wakeup
-    0x64,                                                   // MaxPower: 100mA
+    0x32,                                                   // MaxPower: 100mA
 
-    /* Interface Descriptor (Bulk) */
+    /* Interface Descriptor (HIDAPI) */
     0x09,                                                   // bLength
     0x04,                                                   // bDescriptorType
     0x00,                                                   // bInterfaceNumber
     0x00,                                                   // bAlternateSetting
-    0x02,                                                   // bNumEndpoints
-    0xff,                                                   // bInterfaceClass
-    0xff,                                                   // bInterfaceSubClass
-    0xff,                                                   // bInterfaceProtocol: Other
-    0x03,                                                   // iInterface
+    0x01,                                                   // bNumEndpoints
+    0x03,                                                   // bInterfaceClass
+    0x00,                                                   // bInterfaceSubClass
+    0xff,                                                   // bInterfaceProtocol: OTher
+    0x00,                                                   // iInterface
 
-    /* Endpoint Descriptor (Bulk) */
+    /* HID Descriptor (HIDAPI) */
+    0x09,                                                   // bLength
+    0x21,                                                   // bDescriptorType
+    0x10, 0x01,                                             // bcdHID
+    0x00,                                                   // bCountryCode
+    0x01,                                                   // bNumDescriptors
+    0x22,                                                   // bDescriptorType
+    sizeof(HIDAPIRepDesc), 0x00,                            // wDescriptorLength
+
+    /* Endpoint Descriptor (HIDAPI) */
     0x07,                                                   // bLength
     0x05,                                                   // bDescriptorType
-    0x81,                                                   // bEndpointAddress: IN Endpoint 1 (BULK)
-    0x02,                                                   // bmAttributes
+    0x81,                                                   // bEndpointAddress: IN Endpoint 3
+    0x03,                                                   // bmAttributes
     0x40, 0x00,                                             // wMaxPacketSize
-    0x01,                                                   // bInterval: 125uS
-
-    /* Endpoint Descriptor (Bulk) */
-    0x07,                                                   // bLength
-    0x05,                                                   // bDescriptorType
-    0x02,                                                   // bEndpointAddress: OUT Endpoint 2 (BULK)
-    0x02,                                                   // bmAttributes
-    0x40, 0x00,                                             // wMaxPacketSize
-    0x01,                                                   // bInterval: 125uS
+    0xff,                                                   // bInterval: 255mS
 };
 
 struct usb_string_descriptor_struct {
@@ -120,11 +152,18 @@ const static struct descriptor_list_struct {
 } descriptor_list[] = {
 	{0x00000100, device_descriptor, sizeof(device_descriptor)},
 	{0x00000200, config_descriptor, sizeof(config_descriptor)},
+	// interface number // 2200 for hid descriptors.
+	{0x00002200, HIDAPIRepDesc, sizeof(HIDAPIRepDesc)},
+
+	{0x00002100, config_descriptor + 18, 9 }, // Not sure why, this seems to be useful for Windows + Android.
+
 	{0x00000300, (const uint8_t *)&string0, 4},
-	{0x04090301, (const uint8_t *)&string1, sizeof(FUSB_STR_MANUFACTURER)},
-	{0x04090302, (const uint8_t *)&string2, sizeof(FUSB_STR_PRODUCT)},
-	{0x04090303, (const uint8_t *)&string3, sizeof(FUSB_STR_SERIAL)}
+	{0x04090301, (const uint8_t *)&string1, string1.bLength},
+	{0x04090302, (const uint8_t *)&string2, string2.bLength},
+	{0x04090303, (const uint8_t *)&string3, string3.bLength}
 };
 #define DESCRIPTOR_LIST_ENTRIES ((sizeof(descriptor_list))/(sizeof(struct descriptor_list_struct)) )
 
+
 #endif
+
